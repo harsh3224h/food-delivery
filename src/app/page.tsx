3e6f4b92ed1b268e263e0453122f641162e82b5f
Harsh <harshdhiman7044@gmail.com>
2026-08-17 00:05:55 +0530

    removed the old source code

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Dashboard from "../components/Dashboard";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col">
-      <Dashboard />
-    </main>
-  );
-}
